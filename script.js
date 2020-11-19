379685e3efe54139c3858002f9b1fa76f78f3ba9
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-19 20:28:34 +0100

    Add comments to understand the functions created

--- script.js
+++ script.js
@@ -6,6 +6,14 @@
     let d = 1;
     let sumCards;
     let sumDealer;
+    let dealerCards = [];
+
+    // event listeners
+    document.getElementById("getCard").addEventListener("click", randomCard);
+    document.getElementById("newGame").addEventListener("click", restart);
+    document.getElementById("stopTurn").addEventListener("click", dealerCard);
+
+
 
     // Function to get a random card  
     function randomCard() {
@@ -19,25 +27,22 @@
         delete card;
     }
 
-    document.getElementById("getCard").addEventListener("click", randomCard);
 
 
+    // Function to calculate the sum of the users cards
     function getSum() {
         sumCards = userCards.reduce((sum, currentValue) =>
             sum + currentValue);
 
         if (sumCards == 21) {
-            alert("You win!")
+            alert("That's 21!! Wow, great, you win!")
         } else if (sumCards > 21) {
-            alert("You Lose!")
+            alert("That's more than 21... You Lose!")
         }
     }
 
-    document.getElementById("stopTurn").addEventListener("click", dealerCard);
 
-
-    dealerCards = [0];
-
+    // Function to get random cards for the dealer
     function dealerCard() {
         const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
         let card = cards[Math.floor(Math.random() * cards.length)];
@@ -49,15 +54,13 @@
     }
 
 
-
+    // Function to calculate the sum of the dealer cards + calculate who wins
     function dealersTurn() {
 
         sumDealer = dealerCards.reduce((sum, currentValue) =>
             sum + currentValue);
 
-        if (sumDealer == 21 || sumDealer == sumCards || ) {
-            alert("Dealer wins! That means you lose... ");
-        } else if (sumDealer > sumCards && sumDealer < 21) {
+        if (sumDealer == 21 || sumDealer == sumCards || sumDealer > sumCards && sumDealer < 21) {
             alert("Dealer wins! That means you lose... ");
         } else if (sumDealer < sumCards) {
             dealerCard();
@@ -68,15 +71,12 @@
     }
 
 
-
-
-
+    // function to refresh the page, to start a new game
     function restart() {
         window.location.reload();
     }
 
 
-    document.getElementById("newGame").addEventListener("click", restart);
 
 
 })();
